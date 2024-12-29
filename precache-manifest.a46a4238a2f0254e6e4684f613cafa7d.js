9dda2c09acad33dc89fb9be83640263954c77f74
asats21 <satsalpha@proton.me>
2024-12-29 13:12:57 +0300

    Updates

---
+++ precache-manifest.a46a4238a2f0254e6e4684f613cafa7d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c98d7acd650a2b4173f58972cb25c6fc",
+    "url": "/rare-stats/index.html"
+  },
+  {
+    "revision": "46aabdf6fd6a1a60e00a",
+    "url": "/rare-stats/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "a17f9286d6412c6222bc",
+    "url": "/rare-stats/static/js/2.e5e77c18.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/rare-stats/static/js/2.e5e77c18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c37453c429bc16cf38a",
+    "url": "/rare-stats/static/js/3.7ab2aa13.chunk.js"
+  },
+  {
+    "revision": "46aabdf6fd6a1a60e00a",
+    "url": "/rare-stats/static/js/main.e61b6f52.chunk.js"
+  },
+  {
+    "revision": "a79e89c5aca736c4e9e4",
+    "url": "/rare-stats/static/js/runtime-main.d3229c39.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/rare-stats/static/media/logo.06e73328.svg"
+  }
+]);
